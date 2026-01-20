2eb6455e9593f980c6bfd6698d93ca3fdecb6f52
IdushaGaravi <idushagaravi@gmail.com>
2026-01-20 23:37:20 +0530

    Add forebase

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,20 +1,31 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Spinner } from '@chakra-ui/react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import { useLocation } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase/firebase';
+import Navbar from '../../components/Navbar/Navbar';
 
 const PageLayout = ({children}) => {
     const {pathname} = useLocation();
+    const [user, loading] = useAuthState(auth);
+    const canRenderSideBar = pathname !== "/auth" && user;
+    const canRenderNavBar = !user && !loading && pathname !== "/auth";
+
+    const checkingUserIsAuth = !user && loading;
+    if(checkingUserIsAuth) return <PageLayoutSpinner />;
+
     return (
-        <Flex>
+        <Flex flexDir={canRenderNavBar ? "column" : "row"}>
             {/* Side bar on the left */}
-            {pathname !== '/auth' ? (
+            {canRenderSideBar ? (
                 <Box w={{base: "70px", md: "240px"}}>
                     <Sidebar />
                 </Box>
             ) : null}
-            
+            {/* Navbar */}
+            {canRenderNavBar ? <Navbar /> : null}
             {/* Page content on the right*/}
-            <Box flex={1} w={{base: "calc(100% - 70px)", md: "calc(100% - 240px)"}}>
+            <Box flex={1} w={{base: "calc(100% - 70px)", md: "calc(100% - 240px)"}} mx={"auto"}>
                 {children}
             </Box>
 
@@ -22,4 +33,12 @@ const PageLayout = ({children}) => {
     )
 }
 
-export default PageLayout
+export default PageLayout;
+
+const PageLayoutSpinner = () => {
+    return (
+        <Flex flexDir='column' h='100vh' alignItems='center' justifyContent='center'>
+            <Spinner size='xl' />
+        </Flex>
+    );
+};
