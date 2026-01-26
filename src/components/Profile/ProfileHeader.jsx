fba497af2006811da5e9724483dda0c7e31cc9f4
IdushaGaravi <idushagaravi@gmail.com>
2026-01-26 10:34:51 +0530

    Search user profiles

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,8 +1,18 @@
-import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react"
-import useUserProfileStore from "../../store/userProfileStore"
+import { Avatar, AvatarGroup, Button, Flex, Text, useDisclosure, VStack } from "@chakra-ui/react";
+import useUserProfileStore from "../../store/userProfileStore";
+import useAuthStore from '../../store/authStore';
+import EditProfile from "./EditProfile";
+import useFollowUser from "../../hooks/useFollowUser";
 
 const ProfileHeader = () => {
     const {userProfile} = useUserProfileStore();
+    const authUser = useAuthStore(state => state.user);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isFollowing, isUpdating, handleFollowUser} = useFollowUser(userProfile?.uid);
+
+    const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
+    const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
+
     return (
         <Flex gap={{base: 4, sm: 10}} py={10} direction={{base: "column", sm: "row"}}>
             <AvatarGroup size={{base: "xl", md: "2xl"}} justifySelf={"center"} alignSelf={"flex-start"} mx={"auto"}>
@@ -19,11 +29,28 @@ const ProfileHeader = () => {
                 >
                     <Text fontSize={{base: "sm", md: "lg"}}>{userProfile.username}</Text>
 
-                    <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
-                        <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base: "xs", md: "sm"}}>
-                            Edit Profile
-                        </Button>
+                    {visitingOwnProfileAndAuth && (
+                        <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+                            <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base: "xs", md: "sm"}} onClick={onOpen}>
+                                Edit Profile
+                            </Button>
                     </Flex>
+                    )}  
+
+                    {visitingAnotherProfileAndAuth && (
+                        <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+                            <Button
+                              bg={"blue.500"} 
+                              color={"white"} 
+                              _hover={{bg:"blue.600"}} 
+                              size={{base: "xs", md: "sm"}}
+                              onClick={handleFollowUser}
+                              isLoading={isUpdating}
+                            >
+                                {isFollowing ? "Unfollow" : "Follow"}
+                            </Button>
+                    </Flex>
+                    )}  
                 </Flex>
 
                 <Flex alignItems={"center"} gap={{base: 2, sm: 4}}>
@@ -45,6 +72,7 @@ const ProfileHeader = () => {
                 </Flex>
                 <Text fontSize={"sm"}>{userProfile.bio}</Text>
             </VStack>
+            {isOpen && <EditProfile isOpen={isOpen} onClose={onClose} />}
         </Flex>
     );
 };
